35655f07e8c961033ba9ff8d71f5220f35a28273
Mandorik <arthur_jakobi@hotmail.com>
2025-08-16 15:35:57 -0300

    Inserting user with POST

--- src/main/java/com/mandorik/workshopmongo/services/UserService.java
+++ src/main/java/com/mandorik/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mandorik.workshopmongo.domain.User;
+import com.mandorik.workshopmongo.dto.UserDTO;
 import com.mandorik.workshopmongo.repository.UserRepository;
 import com.mandorik.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id){
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO (UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
