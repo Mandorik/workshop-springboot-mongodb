be14d946e66d7d88b927a30cb7e8ce3d99ce3dff
Mandorik <arthur_jakobi@hotmail.com>
2025-08-19 17:02:10 -0300

    Simple query with query methods

--- src/main/java/com/mandorik/workshopmongo/services/PostService.java
+++ src/main/java/com/mandorik/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mandorik.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,8 @@ public class PostService {
 		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
