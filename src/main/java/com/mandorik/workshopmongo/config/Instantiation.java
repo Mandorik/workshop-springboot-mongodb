4b0b83e5ab9b686c3e73a37875f81f29b5ef2eeb
Mandorik <arthur_jakobi@hotmail.com>
2025-08-19 15:15:09 -0300

    Creating Post entity with nested user

--- src/main/java/com/mandorik/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mandorik/workshopmongo/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.mandorik.workshopmongo.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mandorik.workshopmongo.domain.Post;
 import com.mandorik.workshopmongo.domain.User;
+import com.mandorik.workshopmongo.repository.PostRepository;
 import com.mandorik.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +18,26 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, Instant.parse("2018-03-21T13:25:34Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, Instant.parse("2018-03-23T08:07:18Z"), "Bom dia", "Acordei feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		
 	}
 
 }
