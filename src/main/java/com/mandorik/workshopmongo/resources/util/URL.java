7d528d7a0ff69ceeaef1be991f099465ed84d03d
Mandorik <arthur_jakobi@hotmail.com>
2025-08-19 18:43:29 -0300

    Query with multiple criteria

--- src/main/java/com/mandorik/workshopmongo/resources/util/URL.java
+++ src/main/java/com/mandorik/workshopmongo/resources/util/URL.java
@@ -2,6 +2,9 @@ package com.mandorik.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class URL {
 
@@ -10,6 +13,15 @@ public class URL {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'Z'HH:mm:ss'T'", timezone = "GMT")
+	public static Instant convertDate(String textDate, Instant defaultValue) {
+		try {
+			return Instant.parse(textDate);
+		} catch (Exception e) {
+			return defaultValue;
 		}
 	}
 }
