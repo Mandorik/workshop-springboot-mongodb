aa381e22e823b3a39d6c8c19b2d7a26fbc19db4d
Mandorik <arthur_jakobi@hotmail.com>
2025-08-19 16:12:42 -0300

    Getting a post by its Id

---
+++ src/main/java/com/mandorik/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.mandorik.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mandorik.workshopmongo.domain.Post;
+import com.mandorik.workshopmongo.repository.PostRepository;
+import com.mandorik.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id){
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
